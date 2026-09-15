35c80e471846a78ed362810c51ad377fa5611078
unknown <nastygit@mail.ru>
2026-09-15 09:56:58 +0300

    Lab02 task 1: BMI calculator

--- Program.cs
+++ Program.cs
@@ -19,3 +19,36 @@ byte maxByte = 255;
 byte overflowed = (byte)(maxByte + 1);
 Console.WriteLine($"255 + 1 для byte = {overflowed}");
 
+
+Console. WriteLine( );
+Console.WriteLine("char");
+
+char firstLetter = 'A';
+char separator = '-';
+int charAsNumber = firstLetter; 
+Console.WriteLine($"Символ: {firstLetter}, разделитель:{separator}");
+Console.WriteLine($"Код символа '{firstLetter}' в Unicode: {charAsNumber}") ;
+Console.WriteLine($"Табуляция: \tпосле таба");
+Console.WriteLine($"Перенос: \nпосле переноса");
+
+Console.WriteLine();
+Console.WriteLine("decimal против double");
+double priceDouble = 0.1 + 0.2;
+decimal priceDecimal = 0.1m + 0.2m;
+Console.WriteLine($"double: 0.1 + 0.2 = {priceDouble}");
+Console.WriteLine($"decimal: 0.1 + 0.2 = {priceDecimal}");
+
+
+
+Console.Write("Введите ваш рост в метрах: ");
+string heightInput = Console.ReadLine();
+double hheight = Convert.ToDouble(heightInput);
+
+Console.Write("Введите ваш вес в килограммах: ");
+string weightInput = Console.ReadLine();
+double wweight = Convert.ToDouble(weightInput);
+
+Console.WriteLine($"Рост: {hheight}, Вес: {wweight}"); // <-- диагностика
+
+double imt = wweight / (hheight * hheight);
+Console.WriteLine($"ИМТ: {imt:F2}");
