83dad6ad14bff10e80be708345a7061aa11ab8d3
Настя <nastygit@mail.ru>
2026-09-20 22:18:33 +0300

    rename

--- Lab02/obj/Debug/net10.0/Lab02.AssemblyInfo.cs
+++ Lab02/obj/Debug/net10.0/Lab02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4751e511b12ce8aec0a7497b1683e74a5e3fefd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7988e2723aeeb84cf09d7aeaca11503a98fefa82")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
